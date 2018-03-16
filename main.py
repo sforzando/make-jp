74dc043ef279730cc6a5bced61491b1480ab4765
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-03-16 19:44:08 +0900

    :pushpin: Goodbye world!

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Goodbye, World!')
 
 
 app = webapp2.WSGIApplication([
